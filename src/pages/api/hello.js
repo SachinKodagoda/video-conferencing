b36f02c6fd47b7ee407c243386e4c9ada2d723dc
Sachin Kodagoda <duminda.g.k@gmail.com>
2021-02-26 11:36:30 +0530

    changes upto video camera

---
+++ src/pages/api/hello.js
@@ -0,0 +1,18 @@
+import * as tf from "@tensorflow/tfjs-node";
+
+export default async (req, res) => {
+  try {
+
+    res.status(200).json({
+      success: true,
+      type: "success",
+      message: "successfully added",
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      type: "error",
+      message: "internal error",
+    });
+  }
+};
