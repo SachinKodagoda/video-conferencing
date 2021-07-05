dce7c50436de57a84f4103911f40664a405aa48d
Sachin Kodagoda <duminda.g.k@gmail.com>
2021-07-05 20:03:31 +0530

    no turning back

--- src/common/ui/layouts/MainMiddleArea.tsx
+++ src/common/ui/layouts/MainMiddleArea.tsx
@@ -1,7 +1,9 @@
 import ThreeWorld from '@components/ThreeWorld';
 import { AnimationContext } from '@ctx/AnimationContext';
 import styles from '@layouts_style/MainMiddleArea.module.sass';
-import React, { useContext } from 'react';
+import * as handpose from '@tensorflow-models/handpose';
+import { drawFullHand, getHandCenter } from '@util/handPose';
+import React, { useContext, useEffect } from 'react';
 import Webcam from 'react-webcam';
 
 type TProps = {
@@ -10,8 +12,61 @@ type TProps = {
 };
 
 const MainMiddleArea = ({ canvasRef, webcamRef }: TProps): JSX.Element => {
+  const { setVideoHeight, setVideoWidth, setX, setY, setZoom, setZoomAngle } = useContext(AnimationContext);
   const showWebCam = true;
   const { containerHeight, containerWidth } = useContext(AnimationContext);
+  const runHandpose = async () => {
+    const net = await handpose.load();
+    //  Loop and detect hands
+    setInterval(() => {
+      detect(net);
+    }, 10); // 10
+  };
+
+  const detect = async (net: handpose.HandPose) => {
+    const videoReference = webcamRef?.current?.video as HTMLVideoElement;
+    const canvasReference = canvasRef?.current as HTMLCanvasElement;
+
+    if (
+      typeof webcamRef.current !== 'undefined' &&
+      webcamRef.current !== null &&
+      videoReference.readyState === 4 &&
+      net
+    ) {
+      const { videoWidth } = videoReference;
+      const { videoHeight } = videoReference;
+      videoReference.width = videoWidth;
+      videoReference.height = videoHeight;
+      canvasReference.width = videoWidth;
+      canvasReference.height = videoHeight;
+      setVideoWidth(videoWidth);
+      setVideoHeight(videoHeight);
+
+      const hand = await net.estimateHands(videoReference);
+      const ctx = canvasReference.getContext('2d');
+      if (ctx) {
+        drawFullHand(ctx, hand);
+        // markCanvasCorners(ctx, videoWidth, videoHeight);
+        const xyValues = getHandCenter(hand);
+        if (xyValues.primaryAngle !== null) {
+          setZoomAngle(xyValues.primaryAngle);
+          setZoom(true);
+        } else {
+          setZoom(false);
+        }
+
+        if (xyValues.xVal !== null && xyValues.yVal !== null) {
+          setX(xyValues.xVal);
+          setY(xyValues.yVal);
+        }
+      }
+    }
+  };
+
+  useEffect(() => {
+    runHandpose();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,7 @@ import MainMiddleArea from '@layouts/MainMiddleArea';
 import MainMiddlePlaceHolder from '@layouts/MainMiddlePlaceHolder';
 import MainRightBar from '@layouts/MainRightBar';
 import styles from '@pages_style/index.module.sass';
-import * as handpose from '@tensorflow-models/handpose';
 import '@tensorflow/tfjs-backend-webgl';
-import { drawFullHand, getHandCenter } from '@util/handPose';
 import React, { useContext, useEffect, useRef, useState } from 'react';
 import Webcam from 'react-webcam';
 
@@ -20,59 +18,6 @@ const Index = (): JSX.Element => {
   const [showRightBar, setShowRightBar] = useState(true);
   const { setContainerHeight, setContainerWidth, setVideoHeight, setVideoWidth, setX, setY, setZoom, setZoomAngle } =
     useContext(AnimationContext);
-
-  const runHandpose = async () => {
-    const net = await handpose.load();
-    //  Loop and detect hands
-    setInterval(() => {
-      detect(net);
-    }, 10); // 10
-  };
-
-  const detect = async (net: handpose.HandPose) => {
-    const videoReference = webcamRef?.current?.video as HTMLVideoElement;
-    const canvasReference = canvasRef?.current as HTMLCanvasElement;
-
-    if (
-      typeof webcamRef.current !== 'undefined' &&
-      webcamRef.current !== null &&
-      videoReference.readyState === 4 &&
-      net
-    ) {
-      const { videoWidth } = videoReference;
-      const { videoHeight } = videoReference;
-      videoReference.width = videoWidth;
-      videoReference.height = videoHeight;
-      canvasReference.width = videoWidth;
-      canvasReference.height = videoHeight;
-      setVideoWidth(videoWidth);
-      setVideoHeight(videoHeight);
-
-      const hand = await net.estimateHands(videoReference);
-      const ctx = canvasReference.getContext('2d');
-      if (ctx) {
-        drawFullHand(ctx, hand);
-        // markCanvasCorners(ctx, videoWidth, videoHeight);
-        const xyValues = getHandCenter(hand);
-        if (xyValues.primaryAngle !== null) {
-          setZoomAngle(xyValues.primaryAngle);
-          setZoom(true);
-        } else {
-          setZoom(false);
-        }
-
-        if (xyValues.xVal !== null && xyValues.yVal !== null) {
-          setX(xyValues.xVal);
-          setY(xyValues.yVal);
-        }
-      }
-    }
-  };
-
-  useEffect(() => {
-    runHandpose();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   useEffect(() => {
     const containerRefs = containerRef?.current as HTMLDivElement;
